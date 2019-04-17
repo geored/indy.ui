c70705b8ea6577715982d67bc19897534a20a956
Georgy <ggeorgie@redhat.com>
2019-04-17 09:33:04 +0200

    Cleared unedeeth _/ directory

--- _/subsys/keycloak/target/classes/disabled-keycloak-init.js
+++
@@ -1,6 +0,0 @@
-/* Keycloak authentication is disabled. */
-var auth={loggedIn: false, keycloak: {authenticated: false, loginRequired: false}};
-
-angular.element(document).ready(function () {
-  angular.bootstrap(document, ['indy']);
-});
